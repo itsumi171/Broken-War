c160c328faf610bc8634128676782d75916e2f23
itsumi171 <itsumi.171@gmail.com>
2026-03-03 05:52:57 +0000

    Add lobby scene script

---
+++ scenes/lobby/lobby.gd
@@ -0,0 +1,11 @@
+# Lobby Scene Script
+
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    print("Lobby scene is ready!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
